2c58866300babf405b2f463c002cffc78b0fa464
pol <polmauri5@gmail.com>
2023-12-08 17:20:42 +0100

    feat: :sparkles: add bunch of styles

--- styles/Styles.js
+++ styles/Styles.js
@@ -5,18 +5,39 @@ const styles = StyleSheet.create({
 
     safeAreaView: {
         flex: 1,
-        backgroundColor: "#FAF3FC"
+        margin: 10
     },
     titleText: {
-        fontSize: 64, 
-        fontFamily: "SuezOne-Regular", 
-        color: "#180202",
+        fontSize: 64,
+        fontFamily: "SuezOne-Regular",
         textAlign: "center",
         marginTop: 40
     },
+    titleTextNavBar: {
+        fontSize: 20,
+        fontFamily: "SpaceGrotesk-Bold",
+        textAlign: "center",
+        flex: 1
+    },
+    someText: {
+        fontSize: 13.3,
+        fontFamily: "SpaceGrotesk-Bold",
+    },
+    goBackIcon: {
+        marginRight: 10
+    },
+    navBarView: {
+        display: "flex",
+        flexDirection: "row",
+        width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginTop: 10,
+        marginBottom: 10
+    },
     mainTouchableOpacityLight: {
         display: "flex",
-        width: 380,
+        width: "100%",
         paddingLeft: 12,
         paddingRight: 12,
         paddingTop: 16,
@@ -24,13 +45,25 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         borderRadius: 16,
-        backgroundColor: "#5FB477"
     },
     mainTextOfTouchableOpacityLight: {
-        color: "#180202",
         fontFamily: "SpaceGrotesk-Bold",
         fontSize: 16
-    }
+    },
+    mainPlaceholder: {
+        display: "flex",
+        width: "100%",
+        padding: 16,
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        borderRadius: 16,
+    },
+    mainTextOfPlaceholder: {
+        fontFamily: "SpaceGrotesk-Bold",
+        fontSize: 16,
+        opacity: 0.5
+    },
+
 })
 
 export default styles;
