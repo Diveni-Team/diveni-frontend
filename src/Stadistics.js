10c143ea26453ae66afaaaea2109ba4c45f77c4d
pol <polmauri5@gmail.com>
2023-12-10 19:52:50 +0100

    feat: :sparkles: created play, profile and stadistics screens, no content on it

---
+++ src/Stadistics.js
@@ -0,0 +1,32 @@
+import React, { useState, useContext } from "react";
+import { View, Text, SafeAreaView, TextInput, Switch, TouchableOpacity } from "react-native";
+import styles from "../styles/Styles";
+import themeContext from "../theme/ThemeContext";
+import { useNavigation } from "@react-navigation/native";
+
+function Stadistics() {
+    const theme = useContext(themeContext)
+    const [darkMode, setDarkMode] = useState(false);
+    const navigation = useNavigation();
+
+    const handleNavigationRecoveryEnterNewPassword = () => {
+        navigation.navigate('RecoveryEnterNewPassword');
+    }
+
+    const handleNavigationRecoveryEnterMail = () => {
+        navigation.navigate("RecoveryEnterMail")
+    }
+
+    return (
+        <View style={[styles.viewContainer, { backgroundColor: theme.backgroundColor }]}>
+            <SafeAreaView style={[styles.safeAreaView, { backgroundColor: theme.backgroundColor }]}>
+
+                <Text style={styles.someText}>
+                    Stadistics
+                </Text>
+            </SafeAreaView>
+        </View>
+    );
+}
+
+export default Stadistics;
